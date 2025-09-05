dfcc9df4d2274bb2d431a55a31ec44d3256b3bf3
Akash M <gamkers@Akashs-Mac-mini.local>
2025-09-05 19:49:40 +0530

    Update base URL for FlawHunt repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-10/',
+  base: '/FlawHunt/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
